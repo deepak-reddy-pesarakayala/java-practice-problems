8900cac98bb3b166a15593f66031878958b3c255
deepakreddy960 <pesarakayaladeepakreddy@gmail.com>
2026-08-04 23:21:21 +0530

    feat: DivideTwoIntegers added

---
+++ practice-problems/java-practice-problems/src/practice/DivideTwoIntegers.java
@@ -0,0 +1,9 @@
+package practice;
+public class DivideTwoIntegers {
+    public static void main(String[] args) {
+        int dividend = 10;
+        int divisor = 3;
+        int result = dividend / divisor;
+        System.out.println("Quotient = " + result);
+    }
+}
