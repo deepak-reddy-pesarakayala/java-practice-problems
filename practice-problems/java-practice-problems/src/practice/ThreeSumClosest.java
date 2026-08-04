be70f54ae9b6979255c884f653bd77636fbc9de8
deepakreddy960 <pesarakayaladeepakreddy@gmail.com>
2026-08-04 23:01:56 +0530

    feat: ThreeSumClosest added

---
+++ practice-problems/java-practice-problems/src/practice/ThreeSumClosest.java
@@ -0,0 +1,20 @@
+package practice;
+public class ThreeSumClosest {
+    public static void main(String[] args) {
+        int[] nums = {-1, 2, 1, -4};
+        int target = 1;
+        int closestSum = nums[0] + nums[1] + nums[2];
+        for (int i = 0; i < nums.length - 2; i++) {
+            for (int j = i + 1; j < nums.length - 1; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if (Math.abs(target - sum) <
+                            Math.abs(target - closestSum)) {
+                        closestSum = sum;
+                    }
+                }
+            }
+        }
+        System.out.println("Closest Sum = " + closestSum);
+    }
+}
