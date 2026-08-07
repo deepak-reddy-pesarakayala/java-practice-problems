dacf33cc138d4753cffdbadf3a006168d9566966
deepakreddy960 <pesarakayaladeepakreddy@gmail.com>
2026-08-07 20:42:44 +0530

    feat: SubSets added

---
+++ practice-problems/java-practice-problems/src/practice/SubSets.java
@@ -0,0 +1,24 @@
+package practice;
+import java.util.*;
+public class SubSets {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(0, nums, new ArrayList<>(), result);
+        return result;
+    }
+    private void backtrack(int index, int[] nums,
+                           List<Integer> current,
+                           List<List<Integer>> result) {
+        result.add(new ArrayList<>(current));
+        for (int i = index; i < nums.length; i++) {
+            current.add(nums[i]);
+            backtrack(i + 1, nums, current, result);
+            current.remove(current.size() - 1);
+        }
+    }
+    public static void main(String[] args) {
+        SubSets obj = new SubSets();
+        int[] nums = {1, 2, 3};
+        System.out.println(obj.subsets(nums));
+    }
+}
