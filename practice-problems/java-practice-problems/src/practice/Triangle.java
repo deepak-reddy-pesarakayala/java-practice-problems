1196b43ea57053c3e8ddae82ce4b97914dd73f05
deepakreddy960 <pesarakayaladeepakreddy@gmail.com>
2026-08-07 21:10:50 +0530

    feat: Triangle added

---
+++ practice-problems/java-practice-problems/src/practice/Triangle.java
@@ -0,0 +1,28 @@
+package practice;
+import java.util.*;
+public class Triangle {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++) {
+            dp[i] = triangle.get(n - 1).get(i);
+        }
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                dp[j] = triangle.get(i).get(j)
+                        + Math.min(dp[j], dp[j + 1]);
+            }
+        }
+        return dp[0];
+    }
+    public static void main(String[] args) {
+        Triangle obj = new Triangle();
+        List<List<Integer>> triangle = Arrays.asList(
+                Arrays.asList(2),
+                Arrays.asList(3, 4),
+                Arrays.asList(6, 5, 7),
+                Arrays.asList(4, 1, 8, 3)
+        );
+        System.out.println(obj.minimumTotal(triangle));
+    }
+}
