56881236fe7cbc0ebca3db0e89f274bca8b9a5e7
deepakreddy960 <pesarakayaladeepakreddy@gmail.com>
2026-08-08 20:49:45 +0530

    feat: SortColors added

---
+++ practice-problems/java-practice-problems/src/practice/SortColors.java
@@ -0,0 +1,31 @@
+package practice;
+import java.util.Arrays;
+public class SortColors {
+    public void sortColors(int[] nums) {
+        int low = 0;
+        int mid = 0;
+        int high = nums.length - 1;
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        SortColors obj = new SortColors();
+        int[] nums = {2, 0, 2, 1, 1, 0};
+        obj.sortColors(nums);
+        System.out.println(Arrays.toString(nums));
+    }
+}
