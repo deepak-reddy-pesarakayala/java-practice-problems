e3835f1ee1b6e6881c4bdfd27f5a5283d4a1353e
deepakreddy960 <pesarakayaladeepakreddy@gmail.com>
2026-08-11 17:30:27 +0530

    feat: CompareVersionNumbers added

---
+++ practice-problems/java-practice-problems/src/practice/CompareVersionNumbers.java
@@ -0,0 +1,35 @@
+package practice;
+public class CompareVersionNumbers {
+    public int compareVersion(String version1, String version2) {
+        String[] v1 = version1.split("\\.");
+        String[] v2 = version2.split("\\.");
+        int n = Math.max(v1.length, v2.length);
+        for (int i = 0; i < n; i++) {
+            int num1 = 0;
+            int num2 = 0;
+            if (i < v1.length) {
+                num1 = Integer.parseInt(v1[i]);
+            }
+            if (i < v2.length) {
+                num2 = Integer.parseInt(v2[i]);
+            }
+            if (num1 > num2) {
+                return 1;
+            }
+            if (num1 < num2) {
+                return -1;
+            }
+        }
+        return 0;
+    }
+    public static void main(String[] args) {
+        CompareVersionNumbers obj =
+                new CompareVersionNumbers();
+        System.out.println(
+                obj.compareVersion("1.01", "1.001")
+        );
+        System.out.println(
+                obj.compareVersion("1.0", "1.1")
+        );
+    }
+}
