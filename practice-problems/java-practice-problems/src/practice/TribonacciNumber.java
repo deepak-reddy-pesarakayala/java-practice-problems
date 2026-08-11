6b8cf048720222491efbbe6962e4be2f920d4d19
deepakreddy960 <pesarakayaladeepakreddy@gmail.com>
2026-08-11 17:17:46 +0530

    feat: TribonacciNumber added

---
+++ practice-problems/java-practice-problems/src/practice/TribonacciNumber.java
@@ -0,0 +1,25 @@
+package practice;
+public class TribonacciNumber {
+    public int tribonacci(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        int a = 0;
+        int b = 1;
+        int c = 1;
+        for (int i = 3; i <= n; i++) {
+            int d = a + b + c;
+            a = b;
+            b = c;
+            c = d;
+        }
+        return c;
+    }
+    public static void main(String[] args) {
+        TribonacciNumber obj = new TribonacciNumber();
+        System.out.println(obj.tribonacci(4));
+    }
+}
