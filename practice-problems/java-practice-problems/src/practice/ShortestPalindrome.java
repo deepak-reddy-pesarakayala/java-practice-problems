b10da296e02e0a8a1d80c2f98d4de86e9d78049a
deepakreddy960 <pesarakayaladeepakreddy@gmail.com>
2026-08-13 19:14:41 +0530

    feat: ShortestPalindrome added

---
+++ practice-problems/java-practice-problems/src/practice/ShortestPalindrome.java
@@ -0,0 +1,19 @@
+package practice;
+public class ShortestPalindrome {
+    public String shortestPalindrome(String s) {
+        String reverse = new StringBuilder(s).reverse().toString();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.substring(0, s.length() - i)
+                    .equals(reverse.substring(i))) {
+                return reverse.substring(0, i) + s;
+            }
+        }
+        return "";
+    }
+    public static void main(String[] args) {
+        ShortestPalindrome obj = new ShortestPalindrome();
+        System.out.println(
+                obj.shortestPalindrome("aacecaaa")
+        );
+    }
+}
