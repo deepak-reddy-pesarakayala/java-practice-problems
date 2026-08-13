37f5155aa79a7fe36de6f49afb42c49d2ccfb64b
deepakreddy960 <pesarakayaladeepakreddy@gmail.com>
2026-08-13 20:24:59 +0530

    feat: TablesOfANumber added

---
+++ practice-problems/java-practice-problems/src/practice/TabsOfANumber.java
@@ -0,0 +1,9 @@
+package practice;
+class TablesOfANumber {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " x " + i + " = " + (n * i));
+        }
+    }
+}
