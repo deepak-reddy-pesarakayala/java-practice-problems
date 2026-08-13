7ee441838fafbadb49e492c3057eb84886c2a3ee
deepakreddy960 <pesarakayaladeepakreddy@gmail.com>
2026-08-13 20:21:26 +0530

    feat: SwitchingOfANumber added

---
+++ practice-problems/java-practice-problems/src/practice/SwtchingOfAnumber.java
@@ -0,0 +1,12 @@
+package practice;
+class SwitchingOfANumber {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
