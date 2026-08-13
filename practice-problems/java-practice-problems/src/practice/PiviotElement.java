cbb17e6ae769bd127d6c84b0548f8a6394e6fb4d
deepakreddy960 <pesarakayaladeepakreddy@gmail.com>
2026-08-13 20:05:30 +0530

    feat: PiviotElement added

---
+++ practice-problems/java-practice-problems/src/practice/PiviotElement.java
@@ -0,0 +1,23 @@
+package practice;
+class PivotElement {
+    public int pivotIndex(int[] nums) {
+        int total = 0;
+        for (int num : nums) {
+            total += num;
+        }
+        int leftSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int rightSum = total - leftSum - nums[i];
+            if (leftSum == rightSum) {
+                return i;
+            }
+            leftSum += nums[i];
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        PivotElement obj = new PivotElement();
+        int[] nums = {1, 7, 3, 6, 5, 6};
+        System.out.println(obj.pivotIndex(nums));
+    }
+}
