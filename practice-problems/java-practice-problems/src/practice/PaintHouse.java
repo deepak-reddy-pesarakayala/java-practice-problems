bdee1bad4da9f9088d3a08c44c835fd38ca25deb
deepakreddy960 <pesarakayaladeepakreddy@gmail.com>
2026-08-15 19:22:06 +0530

    feat: PaintHouse added

---
+++ practice-problems/java-practice-problems/src/practice/PaintHouse.java
@@ -0,0 +1,27 @@
+package practice;
+public class PaintHouse {
+    public int minCost(int[][] costs) {
+        if (costs.length == 0) {
+            return 0;
+        }
+        for (int i = 1; i < costs.length; i++) {
+            costs[i][0] += Math.min(costs[i - 1][1], costs[i - 1][2]);
+            costs[i][1] += Math.min(costs[i - 1][0], costs[i - 1][2]);
+            costs[i][2] += Math.min(costs[i - 1][0], costs[i - 1][1]);
+        }
+        int n = costs.length;
+        return Math.min(
+                costs[n - 1][0],
+                Math.min(costs[n - 1][1], costs[n - 1][2])
+        );
+    }
+    public static void main(String[] args) {
+        PaintHouse obj = new PaintHouse();
+        int[][] costs = {
+                {17, 2, 17},
+                {16, 16, 5},
+                {14, 3, 19}
+        };
+        System.out.println(obj.minCost(costs));
+    }
+}
