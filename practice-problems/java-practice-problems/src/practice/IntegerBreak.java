5f1ab28c67a4b2d9f78c316386763f0218feb465
deepakreddy960 <pesarakayaladeepakreddy@gmail.com>
2026-08-15 20:06:38 +0530

    feat: IntegerBreak  added

---
+++ practice-problems/java-practice-problems/src/practice/IntegerBreak.java
@@ -0,0 +1,23 @@
+package practice;
+public class IntegerBreak {
+    public int integerBreak(int n) {
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                dp[i] = Math.max(
+                        dp[i],
+                        Math.max(
+                                j * (i - j),
+                                j * dp[i - j]
+                        )
+                );
+            }
+        }
+        return dp[n];
+    }
+    public static void main(String[] args) {
+        IntegerBreak obj = new IntegerBreak();
+        System.out.println(obj.integerBreak(10));
+    }
+}
