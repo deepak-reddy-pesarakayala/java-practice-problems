039f3f9ce32528879b2747dc88d3163f5ddf4afe
deepakreddy960 <pesarakayaladeepakreddy@gmail.com>
2026-08-15 20:13:00 +0530

    feat: SumOfLeafLeaves  added

---
+++ practice-problems/java-practice-problems/src/practice/SumOfLeafLeaves.java
@@ -0,0 +1,36 @@
+package practice;
+public class SumOfLeafLeaves {
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int val) {
+            this.val = val;
+        }
+    }
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int sum = 0;
+        if (root.left != null) {
+            if (root.left.left == null &&
+                    root.left.right == null) {
+                sum += root.left.val;
+            } else {
+                sum += sumOfLeftLeaves(root.left);
+            }
+        }
+        sum += sumOfLeftLeaves(root.right);
+        return sum;
+    }
+    public static void main(String[] args) {
+        SumOfLeafLeaves obj = new SumOfLeafLeaves();
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+        System.out.println(obj.sumOfLeftLeaves(root));
+    }
+}
