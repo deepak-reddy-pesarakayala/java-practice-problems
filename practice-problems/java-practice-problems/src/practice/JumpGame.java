d2b2d640a1021dbc2a89bed26364891a73a75521
deepakreddy960 <pesarakayaladeepakreddy@gmail.com>
2026-08-16 15:36:19 +0530

    feat: JumpGame  added

---
+++ practice-problems/java-practice-problems/src/practice/JumpGame.java
@@ -0,0 +1,24 @@
+package practice;
+public class JumpGame {
+    public boolean canJump(int[] nums) {
+        int maxReach = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > maxReach) {
+                return false;
+            }
+            maxReach = Math.max(
+                    maxReach,
+                    i + nums[i]
+            );
+            if (maxReach >= nums.length - 1) {
+                return true;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        JumpGame obj = new JumpGame();
+        int[] nums = {2, 3, 1, 1, 4};
+        System.out.println(obj.canJump(nums));
+    }
+}
