08c50d2ccfdf71a400f59f10761a7d169911532e
deepakreddy960 <pesarakayaladeepakreddy@gmail.com>
2026-08-20 15:41:32 +0530

    feat: FibonacciNumber  added

---
+++ practice-problems/java-practice-problems/src/practice/FibonacciNumber.java
@@ -0,0 +1,18 @@
+package practice;
+public class FibonacciNumber {
+    public int fib(int n) {
+        if(n<=1) return n;
+        int a=0;
+        int b=1;
+        for(int i=2;i<=n;i++) {
+            int c=a+b;
+            a=b;
+            b=c;
+        }
+        return b;
+    }
+    public static void main(String[] args) {
+        FibonacciNumber obj=new FibonacciNumber();
+        System.out.println(obj.fib(6));
+    }
+}
